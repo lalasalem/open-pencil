c9e01ba1b67985a0f6b338192f5bebf15d0d50b2
lalasalem <leen.saleh007@outlook.com>
2026-04-24 18:57:13 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
 
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, './src'),
+
       '@tauri-apps/api/core': '/src/shims/tauri.ts',
       'reka-ui': '/src/shims/reka-ui.ts',
       'canvaskit-wasm': '/src/shims/canvaskit.ts'
